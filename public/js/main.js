951262e2228d6997751654ac90e2595636070c2f
Daniel Dias <danielnunes@hotmail.com>
2018-07-22 22:28:53 -0400

    Implemented more of the game logic and styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,21 @@
-var sentence = $(".sentence").text();
-var numberOfWords = sentence.split(" ").length;
-$('#numberOfWords').text(numberOfWords);
-
+var gameTime = 3;
 var userInputField = $('#userInput');
 
-userInputField.on("input", function() {
+$(function() {
+    $('#timeLeft').text(gameTime);
+    userInputField.on("input", updateTypedInfo);
+    $('#restartButton').click(startGame);
+    updateSentenceNumber();
+    startGame();
+})
 
+
+
+function updateSentenceNumber() {
+    $('#numberOfWords').text($(".sentence").text().split(" ").length);
+}
+
+function updateTypedInfo() {
     var typed = userInputField.val();
 
     var typedCharacters = typed.length;
@@ -14,18 +24,42 @@ userInputField.on("input", function() {
     var typedWords = typed.split(/\S+/g).length - 1;
     $('#typedWords').text(typedWords);
 
-});
+    checkStatus(typed);
+}
 
-var timeLeft = $('#timeLeft').text();
+function startGame() {
+    timeLeft = gameTime;
+    userInputField.attr("disabled", false);
+    userInputField.val('');
+    $('#timeLeft').text(timeLeft);
+    $('#typedCharacters').text(0);
+    $('#typedWords').text(0);
+    userInputField.removeClass("disabledBox"); 
+    userInputField.removeClass("wrong");
+    userInputField.removeClass("right");
+    $('#restartButton').attr("disabled", true);
+    userInputField.one("focus", function() {
+        startClock()
+    })
+}
 
-
-
-userInputField.on("focus", function() {
-    var decreaseTime = setInterval(function() {
-        $('#timeLeft').text(timeLeft--);
-        if (timeLeft < 0) {
+function startClock() {
+    decreaseTime = setInterval(function() {
+        $('#timeLeft').text(this.timeLeft--);
+        if (this.timeLeft < 0) {
             clearInterval(decreaseTime);
             userInputField.attr("disabled", true);
+            $('#restartButton').attr("disabled", false);
+            userInputField.addClass("disabledBox");
         }
     }, 1000);
-})
+}
+
+function checkStatus(typed) {
+    var comparableSentence = $('.sentence').text().substr(0, typed.length);
+
+    var isRight = (typed == comparableSentence);
+
+    userInputField.toggleClass("right", isRight);
+    userInputField.toggleClass("wrong", !isRight);
+}
